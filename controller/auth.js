ca39fd3b82abfa496122e9cede6f9ad99d1a01c8
TruongNT <truongnt267@gmail.com>
2025-04-30 20:01:00 +0700

    modifind file name

--- controller/auth.js
+++ controller/auth.js
@@ -1,11 +1,61 @@
+import bcrypt from "bcrypt";
 import db from "../models/index.js";
 
-const login = async (req, res) => {
-  const new_user = await db.users.create({
-    username: "admin",
-    password: "admin",
+const register = async (req, res) => {
+  const { username, password, fullname } = req.body;
+  const user = await db.users.findOne({
+    where: {
+      username: username,
+    },
   });
-  return res.status(200).json(new_user);
+  if (user) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const newUser = await db.users.create({
+    ...req.body,
+    password: hashedPassword,
+  });
+
+  return res.status(201).json({
+    message: "Đăng ký thành công!",
+    data: {
+      fullname: newUser.fullname,
+      role: newUser.role,
+      username: newUser.username,
+    },
+  });
 };
 
-export { login };
+const login = async (req, res) => {
+  const user = await db.users.findOne({
+    where: {
+      username: req.body.username,
+    },
+  });
+
+  if (user) {
+    console.log(user);
+    const comparePass = bcrypt.compare(req.body.password, user.password);
+    if (!comparePass) {
+      return res.status(401).json({
+        message: "Tài khoản hoặc mật khẩu không chính xác.",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Đăng nhập thành công.",
+      data: {
+        fullname: user.fullname,
+        role: user.role,
+        username: user.username,
+      },
+    });
+  } else {
+    return res.status(401).json({
+      message: "dang nhap that bai",
+    });
+  }
+};
+
+export { register, login };
