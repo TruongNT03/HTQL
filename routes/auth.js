e75d9110522a2bef6311559c38b37ec96938187d
TruongNT <truongnt267@gmail.com>
2025-05-01 02:54:35 +0700

    auth with jwt, add more middleware, change user_role to enum, updateUser route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import * as AuthController from "../controller/auth.js";
+import verifyToken from "../middlewares/verifyToken.js";
+import authorization from "../middlewares/authorization.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 
 const router = Router();
 
 router.post("/register", asyncHandler(AuthController.register));
 router.post("/login", asyncHandler(AuthController.login));
+router.put(
+  "/",
+  verifyToken,
+  authorization(["admin"]),
+  asyncHandler(AuthController.updateUser)
+);
 
 export default router;
